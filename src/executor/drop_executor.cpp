9f536ea07e80e3520bb974ff3478a038dd66d981
PauloAmora <pauloamora1111@gmail.com>
2017-11-16 16:59:40 -0300

    eviction implemented, memory still going up

--- src/executor/drop_executor.cpp
+++ src/executor/drop_executor.cpp
@@ -12,11 +12,15 @@
 
 #include "executor/drop_executor.h"
 
-#include "catalog/index_catalog.h"
 #include "catalog/catalog.h"
 #include "catalog/trigger_catalog.h"
 #include "common/logger.h"
+#include "concurrency/transaction.h"
 #include "executor/executor_context.h"
+#include "planner/drop_plan.h"
+#include "eviction/evicter.h"
+#include "storage/tile_group.h"
+#include "storage/tile_group_header.h"
 
 namespace peloton {
 namespace executor {
@@ -39,25 +43,54 @@ bool DropExecutor::DInit() {
 
 bool DropExecutor::DExecute() {
   LOG_TRACE("Executing Drop...");
-  bool result;
-  const planner::DropPlan &node = GetPlanNode<planner::DropPlan>();
-  auto current_txn = context_->GetTransaction();
-  DropType dropType = node.GetDropType();
-  switch (dropType) {
-    case DropType::DB: {
-      result = DropDatabase(node, current_txn);
-      break;
-    }
-    case DropType::TABLE: {
-      result = DropTable(node, current_txn);
-      break;
-    }
-    case DropType::TRIGGER: {
-      result = DropTrigger(node, current_txn);
-      break;
-    }
-    case DropType::INDEX: {
-      result = DropIndex(node, current_txn);
+  const planner::DropPlan &node UNUSED_ATTRIBUTE = GetPlanNode<planner::DropPlan>();
+  //DropType dropType = node.GetDropType();
+ // switch (dropType) {
+ //   case DropType::TABLE: {
+      auto table_name UNUSED_ATTRIBUTE= node.GetTableName();
+      auto current_txn UNUSED_ATTRIBUTE= context_->GetTransaction();
+
+
+          auto table_object UNUSED_ATTRIBUTE = catalog::Catalog::GetInstance()->GetTableWithName(
+              DEFAULT_DB_NAME, table_name, current_txn);
+          auto evicter = new eviction::Evicter();
+
+        for (int i = 1; i<50; i++)
+        table_object->GetTileGroup(i)->GetHeader()->SetEvictable(true);
+        evicter->EvictDataFromTable(table_object);
+      delete evicter;
+      current_txn->SetResult(ResultType::SUCCESS);
+
+      if (current_txn->GetResult() == ResultType::SUCCESS) {
+        LOG_TRACE("Dropping table succeeded!");
+      } else {
+        LOG_TRACE("Result is: %s",
+                  ResultTypeToString(current_txn->GetResult()).c_str());
+      }
+
+    /*case DropType::TRIGGER: {
+      std::string table_name = node.GetTableName();
+      std::string trigger_name = node.GetTriggerName();
+
+      auto current_txn = context_->GetTransaction();
+
+      ResultType result = catalog::TriggerCatalog::GetInstance()->DropTrigger(
+          DEFAULT_DB_NAME, table_name, trigger_name, current_txn);
+      current_txn->SetResult(result);
+
+      if (current_txn->GetResult() == ResultType::SUCCESS) {
+        LOG_TRACE("Dropping trigger succeeded!");
+      } else if (current_txn->GetResult() == ResultType::FAILURE &&
+                 node.IsMissing()) {
+        current_txn->SetResult(ResultType::SUCCESS);
+        LOG_TRACE("Dropping trigger Succeeded!");
+      } else if (current_txn->GetResult() == ResultType::FAILURE &&
+                 !node.IsMissing()) {
+        LOG_TRACE("Dropping trigger Failed!");
+      } else {
+        LOG_TRACE("Result is: %s",
+                  ResultTypeToString(current_txn->GetResult()).c_str());
+      }
       break;
     }
     default: {
@@ -65,99 +98,7 @@ bool DropExecutor::DExecute() {
           StringUtil::Format("Drop type %d not supported yet.\n", dropType));
     }
   }
-
-  return result;
-}
-
-bool DropExecutor::DropDatabase(const planner::DropPlan &node,
-                                concurrency::TransactionContext *txn) {
-  auto database_name = node.GetDatabaseName();
-  if (node.IsMissing()) {
-    try {
-      auto database_object = catalog::Catalog::GetInstance()->GetDatabaseObject(
-          database_name, txn);
-    } catch (CatalogException &e) {
-      LOG_TRACE("Database %s does not exist.", database_name.c_str());
-      return false;
-    }
-  }
-
-  ResultType result =
-      catalog::Catalog::GetInstance()->DropDatabaseWithName(database_name, txn);
-  txn->SetResult(result);
-
-  if (txn->GetResult() == ResultType::SUCCESS) {
-    LOG_TRACE("Dropping database succeeded!");
-  } else {
-    LOG_TRACE("Result is: %s", ResultTypeToString(txn->GetResult()).c_str());
-  }
-  return false;
-}
-
-bool DropExecutor::DropTable(const planner::DropPlan &node,
-                             concurrency::TransactionContext *txn) {
-  auto database_name = node.GetDatabaseName();
-  auto table_name = node.GetTableName();
-
-  if (node.IsMissing()) {
-    try {
-      auto table_object = catalog::Catalog::GetInstance()->GetTableObject(
-          database_name, table_name, txn);
-    } catch (CatalogException &e) {
-      LOG_TRACE("Table %s does not exist.", table_name.c_str());
-      return false;
-    }
-  }
-
-  ResultType result = catalog::Catalog::GetInstance()->DropTable(
-      database_name, table_name, txn);
-  txn->SetResult(result);
-
-  if (txn->GetResult() == ResultType::SUCCESS) {
-    LOG_TRACE("Dropping table succeeded!");
-  } else {
-    LOG_TRACE("Result is: %s", ResultTypeToString(txn->GetResult()).c_str());
-  }
-  return false;
-}
-
-bool DropExecutor::DropTrigger(const planner::DropPlan &node,
-                               concurrency::TransactionContext *txn) {
-  auto database_name = node.GetDatabaseName();
-  std::string table_name = node.GetTableName();
-  std::string trigger_name = node.GetTriggerName();
-
-  ResultType result = catalog::TriggerCatalog::GetInstance().DropTrigger(
-      database_name, table_name, trigger_name, txn);
-  txn->SetResult(result);
-
-  if (txn->GetResult() == ResultType::SUCCESS) {
-    LOG_TRACE("Dropping trigger succeeded!");
-  } else if (txn->GetResult() == ResultType::FAILURE && node.IsMissing()) {
-    txn->SetResult(ResultType::SUCCESS);
-    LOG_TRACE("Dropping trigger Succeeded!");
-  } else if (txn->GetResult() == ResultType::FAILURE && !node.IsMissing()) {
-    LOG_TRACE("Dropping trigger Failed!");
-  } else {
-    LOG_TRACE("Result is: %s", ResultTypeToString(txn->GetResult()).c_str());
-  }
-  return false;
-}
-
-bool DropExecutor::DropIndex(const planner::DropPlan &node,
-                             concurrency::TransactionContext *txn) {
-  std::string index_name = node.GetIndexName();
-  auto index_object =
-      catalog::IndexCatalog::GetInstance()->GetIndexObject(index_name, txn);
-  ResultType result = catalog::Catalog::GetInstance()->DropIndex(
-      index_object->GetIndexOid(), txn);
-  txn->SetResult(result);
-  if (txn->GetResult() == ResultType::SUCCESS) {
-    LOG_TRACE("Dropping Index Succeeded! Index oid: %d",
-              index_object->GetIndexOid());
-  } else {
-    LOG_TRACE("Dropping Index Failed!");
-  }
+*/
   return false;
 }
 
